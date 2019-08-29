f039ddbe5f30a12619a9b05cc2e3b2547b29a5c2
lamvu <tunglam195@gmail.com>
2019-08-29 13:52:21 +0700

    update test 2

--- application/config/config.php
+++ application/config/config.php
@@ -23,14 +23,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/crfpr/';
+//$config['base_url'] = 'http://localhost/crfpr/';
 /*$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);*/
-/*
-$root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+
+$root=(isset($_SERVER['HTTPS']) ? "https://" : "https://").$_SERVER['HTTP_HOST'];
 $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-$config['base_url'] = $root;*/
+$config['base_url'] = $root;
 /*
 |--------------------------------------------------------------------------
 | Index File
